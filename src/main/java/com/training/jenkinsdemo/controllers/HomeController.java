ddeef343aa1b010c24894f79f97808ca64e5bb75
amdcdac20 <amdcdac20@amdcdac03.iiht.tech>
2022-06-16 10:15:29 +0530

    first commit

---
+++ src/main/java/com/training/jenkinsdemo/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package com.training.jenkinsdemo.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("home")
+public class HomeController {
+	
+	@RequestMapping("/msg")
+	public String message() {
+		return "Hello from SpringB";
+	}
+
+}
